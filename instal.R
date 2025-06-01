aecc18b13c7e35ccfdf6f205d01c11de5e25c269
SJimmyW <163605349+SJimmyW@users.noreply.github.com>
2025-06-01 13:01:36 -0300

    Update instal.R

--- instal.R
+++ instal.R
@@ -1,2 +1,2 @@
 # install.R
-install.packages(c("shiny","dplyr","ggplot2","AlphaSimR", "ggridges", "tidyr", "data.table" ))
+install.packages(c("shiny","dplyr", "stringr", "ggplot2","AlphaSimR", "ggridges", "tidyr", "data.table" ))
